1bac633c780e737a6894eb7068e4c8875277b3f5
patriciagilbon <paty.gilbon@me.com>
2022-08-23 11:57:18 +0200

    crud added

--- routes/index.js
+++ routes/index.js
@@ -26,4 +26,42 @@ router.get('/movies', (req, res, next) => {
     .catch(err => console.log(`The details were not retrieved: ${err}`))
   })
 
+  router.get('/create', (req, res, next) =>{
+    res.render('movie-create');
+  })
+
+  router.post('/create', (req, res, next) =>{
+    const {title, director, description, stars} = req.body;
+
+    Movie.create({title, director, description, stars})
+    .then(() => res.redirect('/movies'))
+    .catch(error => `Error while creating a new book: ${error}`);
+  })
+
+  router.get('/movies/:id/edit', (req, res, next) =>{
+    const {id} = req.params;
+    console.log(id)
+    Movie.findById(id)
+    .then(MovieToEdit => {
+        res.render('movie-edit', MovieToEdit);
+        console.log(MovieToEdit)
+    })
+    .catch(err => console.log(`The details were not retrieved: ${err}`))
+  })
+
+  router.post('/movies/:id/edit', (req, res, next) =>{
+    const {id} = req.params;
+    const {title, director, description, stars} = req.body;
+
+    Movie.findByIdAndUpdate(id, {title, director, description, stars}, { new: true })
+    .then((UpdatedMovie) => res.redirect(`/movies/${UpdatedMovie._id}`))
+    .catch(err => console.log(`The details were not retrieved: ${err}`))
+  })
+
+  router.get('/movies/:id/delete', (req, res, next) =>{
+    const {id} = req.params;
+    Movie.findByIdAndDelete(id)
+    .then(() => res.redirect('/movies'))
+    .catch(error => console.log(`The movie could not be deleted ${err}`))
+  })
 module.exports = router;
